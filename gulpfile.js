b284453bbed3c777fb5c608cab47175c1d253eb4
fix777 <wangguoguo.lan@gmail.com>
2017-07-20 19:31:48 +0800

    add less build support

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 const gulp = require("gulp");
 const gts = require("gulp-typescript");
-const tsc = require("typescript");
+const gless = require("gulp-less");
 const fs = require("fs");
 const path = require("path");
 const tscOptions = require("./tsconfig.gulp.json").compilerOptions;
 
 const sourcePath = path.resolve(__dirname, "src/components");
 const tsxPaths = fs.readdirSync(sourcePath);
+
+const loopEntries = (curPath, basePath, entries, merged = []) => {
+  entries.forEach(childEntry => {
+    const childEntryPath = path.resolve(basePath, childEntry);
+    const isFile = fs.statSync(childEntryPath).isFile();
+    if (isFile) return merged.push([ curPath, childEntryPath ]);
+    // directory
+    furtherChildEnties = fs.readdirSync(childEntryPath);
+    return loopEntries(`${curPath}/${childEntry}`, path.resolve(basePath, childEntry), furtherChildEnties, merged);
+  });
+  return merged;
+};
+
 const tsxEntries =
   tsxPaths.reduce(
     (entries, curPath) => {
@@ -14,12 +27,12 @@ const tsxEntries =
       const basePath = path.resolve(sourcePath, curPath);
 
       if (curPath.includes("index")) {
-        compEntries.push({ index: basePath });
+        compEntries.push([ "index", basePath ]);
       }
       else {
         // console.log(fs.readdirSync(basePath));
-        const childEntries = fs.readdirSync(basePath)
-        compEntries = childEntries.map(childEntry => ({ [curPath]: path.resolve(basePath, childEntry) }))
+        const childEntries = fs.readdirSync(basePath);
+        compEntries = loopEntries(curPath, basePath, childEntries);
       }
 
       return [...entries, ...compEntries];
@@ -29,17 +42,27 @@ const tsxEntries =
 
 gulp.task("default", () => {
   // console.log(tsxEntries);
-  tsxEntries.forEach((tsxEntry, tsxEntryIndex) => {
-    const [[ entryName, entryPath ]] = Object.entries(tsxEntry);
+  tsxEntries.forEach(([ entryName, entryPath ], tsxEntryIndex) => {
     if (entryName === "index") {
       tscOptions.outDir = path.resolve(__dirname, "components_temp");
     } else {
       tscOptions.outDir = path.resolve(__dirname, "components_temp", entryName);
     }
 
-    gulp
-      .src(entryPath)
-      .pipe(gts(tscOptions))
-      .pipe(gulp.dest(tscOptions.outDir));
+    if (/\.ts?x$/.test(entryPath)) {
+      gulp
+        .src(entryPath)
+        .pipe(gts(tscOptions))
+        .pipe(gulp.dest(tscOptions.outDir));
+    } else if (/\.less$/.test(entryPath)) {
+      gulp
+        .src(entryPath)
+        .pipe(gless())
+        .pipe(gulp.dest(path.resolve(__dirname, "lib", entryName)));
+      // Build both css & less
+      gulp
+        .src(entryPath)
+        .pipe(gulp.dest(path.resolve(__dirname, "lib", entryName)));
+    }
   });
 });
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Button = (props) => (
-  <button>Hello Button, yo yo check now.</button>
+  <button
+    className="rantd-button"
+  >
+    Hello Button, yo yo check now.
+  </button>
 );
 
 export default Button;
